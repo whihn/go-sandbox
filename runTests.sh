03307cee05dfc39919e5b19599ecb8555531eb9c
whihn <werner.hihn@gmail.com>
2016-03-16 22:34:42 +0100

    added color escape sequence to test output

--- runTests.sh
+++ runTests.sh
@@ -1,2 +1,2 @@
 cd /Users/werner/go-workspace/src/github.com/whihn/go-sandbox
-go test ./...
+go test -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
