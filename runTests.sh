bec4be8b61c60d143bf832020ffe0901fafec609
whihn <werner.hihn@gmail.com>
2016-03-21 21:24:42 +0100

    dont use verbose test output, also color "ok" green

--- runTests.sh
+++ runTests.sh
@@ -1,2 +1,2 @@
 cd /Users/werner/go-workspace/src/github.com/whihn/go-sandbox
-go test -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | sed ''/ok/s//$(printf "\033[32mok\033[0m")/''
