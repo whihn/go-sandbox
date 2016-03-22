47a04f850fcc55776241c0d2947c245b12f31195
whihn <werner.hihn@gmail.com>
2016-03-22 20:34:53 +0100

    added mod-time verification

--- fileutils/FileCrawler_test.go
+++ fileutils/FileCrawler_test.go
@@ -4,16 +4,18 @@ import (
 	"../fileutils"
 	"testing"
 	"os"
+	"time"
 )
 
 func TestCreatesFileCrawlerWithRootDirectory(t *testing.T) {
 	fileName := "../testdata"
 	file, err := fileutils.Crawl(fileName)
 	if err != nil {
-		t.Fatal("Error was thrown", err)
+		t.Fatal("crawling directory", fileName, "failed: ", err)
 	}
 	assertEquals(t, fileName, file.Name)
 	assertEquals(t, 1, len(file.FileInfos))
+	assertTimestamp(t, file.FileInfos[0], 2016, time.March, 20, 12, 38)
 }
 
 func TestReadDir(t *testing.T) {
@@ -30,4 +32,13 @@ func assertEquals(t *testing.T, expected, actual interface{}) {
 	if expected != actual {
 		t.Error("Expected", expected, "got", actual)
 	}
+}
+
+func assertTimestamp(t *testing.T, fileInfo os.FileInfo, year int, month time.Month, day int, hour int, minute int) {
+	time := fileInfo.ModTime()
+	assertEquals(t, year, time.Year())
+	assertEquals(t, month, time.Month())
+	assertEquals(t, day, time.Day())
+	assertEquals(t, hour, time.Hour())
+	assertEquals(t, minute, time.Minute())
 }
